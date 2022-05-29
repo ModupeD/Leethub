9361d1765f037e6ab1de80d8939ccf53ec576efc
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-05-29 07:58:18 -0400

    Time: 651 ms (14.16%), Space: 48.9 MB (76.67%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.js
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.js
@@ -3,29 +3,27 @@
  * @return {number}
  */
 var lengthOfLongestSubstringTwoDistinct = function(s) {
-    let windowStart = 0,
-    maxLength = 0,
-    charFrequency = {};
-
-  // in the following loop we'll try to extend the range [window_start, window_end]
-  for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
-    const rightChar = s[windowEnd];
-    if (!(rightChar in charFrequency)) {
-      charFrequency[rightChar] = 0;
+       let max =0,
+      windowStart = 0,
+      progress = {};
+    
+    for(let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+        const rightChar = s[windowEnd];
+        if(!(rightChar in progress)) {
+            progress[rightChar] = 0;
+        }
+        progress[rightChar] += 1;
+        
+        while(Object.keys(progress).length > 2){
+            const leftChar = s[windowStart];
+            progress[leftChar] -=1;
+            if(progress[leftChar] === 0){
+                delete progress[leftChar];
+            }
+            windowStart += 1;
+        }
+        
+        max = Math.max(max, (windowEnd - windowStart) + 1)
     }
-    charFrequency[rightChar] += 1;
-    // shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
-    while (Object.keys(charFrequency).length > 2) {
-      const leftChar = s[windowStart];
-      charFrequency[leftChar] -= 1;
-      if (charFrequency[leftChar] === 0) {
-        delete charFrequency[leftChar];
-      }
-      windowStart += 1; // shrink the window
-    }
-    // remember the maximum length so far
-    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
-  }
-
-  return maxLength;
+    return max
 };
