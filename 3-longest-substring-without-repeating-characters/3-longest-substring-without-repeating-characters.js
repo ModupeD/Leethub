e516b5747ccdf462e3c81019f06768c215a1235a
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-05-29 01:59:51 -0400

    Time: 184 ms (33.85%), Space: 48.6 MB (44.76%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLongestSubstring = function(s) {
+    let max = 0
+    let begin = 0
+    let map = {}
+    for(let end = 0; end < s.length; end ++){
+        if(map[s[end]] !== undefined && map[s[end]] >= begin) {
+            begin = map[s[end]] + 1
+        }
+         map[s[end]] = end
+         max = Math.max(max, end - begin + 1)
+        }
+    
+       return max
+};
