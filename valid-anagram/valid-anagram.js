7ab62899e6b6661a1bff964f521f32e354a9b0ea
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-06-04 08:25:48 -0400

    Time: 172 ms, Memory: 44.7 MB - LeetHub

--- valid-anagram/valid-anagram.js
+++ valid-anagram/valid-anagram.js
@@ -4,24 +4,26 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length){
-        return false
-    }
-
-    let letterCount = {}
     
-    for(let letter of s){
-      if (letterCount[letter] === undefined) {
-            letterCount[letter] = 1;
-      } else {
-            letterCount[letter]++;
-      }
-  }
+        if(s.length !== t.length) return false
+        
+        let letterCount = {};
+        
+        for(let letter of s){
+            if(letterCount[letter] === undefined){
+                letterCount[letter] = 1;
+            } else{
+                letterCount[letter]++;
+            }
+        }
         for(let letter of t){
-            if(letterCount[letter] === undefined) return false
-            if(letterCount[letter] < 1) return false
-            
+            if(letterCount[letter] === undefined){
+               return false
+            } if(letterCount[letter] < 1){
+                return false
+            }
             letterCount[letter]--;
-    }
-    return true;
+        }
+        
+    return true
 };
