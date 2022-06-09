4eaaeb093a424bc87f3ca5eeb420f79b7724f5c1
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-06-09 07:30:24 -0400

    Time: 96 ms, Memory: 46.6 MB - LeetHub

---
+++ validate-binary-search-tree/validate-binary-search-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    return dfs(root, -Infinity, Infinity)
+    
+};
+
+function dfs(node, min, max){
+    if(!node){
+      return true
+    }
+    
+    if(node.val <= min || node.val >= max){
+        return false
+    }
+    
+    return dfs(node.left, min, node.val) && dfs(node.right, node.val, max)
+}
