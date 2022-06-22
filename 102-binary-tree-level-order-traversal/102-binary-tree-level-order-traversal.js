b7da53fa8bde6bb1f7cf5b367d948d948ec6966c
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-06-21 23:08:55 -0400

    Time: 94 ms (46.20%), Space: 44.8 MB (7.37%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -11,22 +11,37 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    let levels = [];
-    if(!root) return levels;
     
-   const queue = [root];
-   const result = []
-   
-   while(queue.length){
-        let len = queue.length;
-        result.push(queue.map(node => node.val))
+    let output = []
+    function getOutput(node, level){
+        if(!node) return 
         
-       while(len--){
-            let node = queue.shift();
-           if(node.left) queue.push(node.left);
-           if(node.right) queue.push(node.right);
-       }
-   }
+        if(output[level]) {
+            output[level].push(node.val)
+        }else{
+            output[level] = [node.val]
+        }
+        getOutput(node.left, level+1)
+        getOutput(node.right, level+1)
+}
+    getOutput(root, 0)
+    return output
+//     let levels = [];
+//     if(!root) return levels;
     
-    return result
+//    const queue = [root];
+//    const result = []
+   
+//    while(queue.length){
+//         let len = queue.length;
+//         result.push(queue.map(node => node.val))
+        
+//        while(len--){
+//             let node = queue.shift();
+//            if(node.left) queue.push(node.left);
+//            if(node.right) queue.push(node.right);
+//        }
+//    }
+    
+//     return result
 };
