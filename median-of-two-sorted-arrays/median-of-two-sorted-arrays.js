1020881ed34434cd82ec533a837ddf45b1a954cc
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-06-23 11:00:59 -0400

    Time: 160 ms, Memory: 48.1 MB - LeetHub

---
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const mergedArr = nums1.concat(nums2).sort((a,b) => a-b);
+    let index = mergedArr.length/2
+    let mergedIndex =  Math.floor(index);
+    
+   if(mergedArr.length % 2 == 0){
+       
+      return  (mergedArr[mergedIndex]+ mergedArr[mergedIndex-1])/2
+       
+   } else {
+       
+         return  mergedArr[mergedIndex]
+       
+   }
+  
+   
+};
