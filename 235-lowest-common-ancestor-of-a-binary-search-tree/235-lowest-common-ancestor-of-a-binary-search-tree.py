9f4f67d5b338ff82f04342ff68251b04d31c2158
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-08-11 23:31:01 -0400

    Time: 84 ms (67.38%), Space: 21.4 MB (78.05%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -12,6 +12,16 @@ class Solution(object):
         :type p: TreeNode
         :type q: TreeNode
         :rtype: TreeNode
+ 
+ p and q are descendants of the node or p or q equal to this node
+ we got 6 because p is less than 6 so it goes in the left subtree, q is greater than 6 so it goes
+ in the right subtree, if there is a split, where that split occurs is the lowest common ancestor
+ 
+ if p =7 and q = 9 then we know that it would be located in the left subtree, because those values 
+ are both greater than the root node, 8 is the lowest common ancestor because that is where the split occured
+ 
+ if p = 7 and q =6, because one of them is equal to the root node none of the descendants would be an ancestor of that node itself. 
+ 
         """
         cur = root
         
