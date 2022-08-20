db4d416afb9b75d8750046c955cc0d14b781f604
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-08-20 14:07:04 -0400

    Time: 2022 ms (45.70%), Space: 13.4 MB (87.63%) - LeetHub

---
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -0,0 +1,27 @@
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        self.n = n
+        celebrity_candidate = 0
+        for i in range(1, n):
+            if knows(celebrity_candidate, i):
+                celebrity_candidate = i
+        if self.is_celebrity(celebrity_candidate):
+            return celebrity_candidate
+        return -1
+
+    def is_celebrity(self, i):
+        for j in range(self.n):
+            if i == j: continue
+            if knows(i, j) or not knows(j, i):
+                return False
+        return True
