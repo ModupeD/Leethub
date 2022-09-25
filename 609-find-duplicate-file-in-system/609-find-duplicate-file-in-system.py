9cfbbefb2aa2a3274a6e20538ab9c0005d6a5645
ModupeD <69601432+ModupeD@users.noreply.github.com>
2022-09-24 20:23:46 -0400

    Time: 260 ms (15.72%), Space: 23 MB (96.93%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        "root/d1/d2/.../dm f1.txt(f1_content) f2.txt(f2_content) ... fn.txt(fn_content)"
+        c = defaultdict(list)
+        #key content value(path name)
+        for path in paths:
+            path = path.split(" ")
+            folder = path[0]
+            for s in path[1:]:
+                s = s.split(".txt")
+                name = s[0]
+                content = s[1]
+                c[content].append((folder, name))
+        output = []
+        for k,v in c.items():
+            if len(v)>1:
+                tmp = []
+                for path, name in v:
+                    tmp.append(path+"/"+ name+".txt")
+                output.append(tmp)
+                
+        return output
+                    
+                    
